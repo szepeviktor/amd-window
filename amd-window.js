4990c5cf0b8f7a2f6774864ed6290ee6bff8a873
Viktor Szépe <viktor@szepe.net>
2014-08-13 21:37:51 +0200

    Create amd-window.js

---
+++ amd-window.js
@@ -0,0 +1,16 @@
+/** @license The MIT License (MIT) Copyright (c) 2014 Viktor Szépe */
+
+/**
+ * AMD window
+ * Helps depending on the window object
+ *
+ * Licensed under the MIT License at:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+define(function () {
+"use strict";
+
+    return window;
+});

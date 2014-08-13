680a144272b3c022324572cae8345c3a2572365f
Viktor Szépe <viktor@szepe.net>
2014-08-13 21:39:35 +0200

    Create amd-window-concatenate.js

---
+++ amd-window-concatenate.js
@@ -0,0 +1,17 @@
+/** @license The MIT License (MIT) Copyright (c) 2014 Viktor Szépe */
+
+/**
+ * AMD window
+ * Helps depending on the window object
+ *
+ * Licensed under the MIT License at:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+// when concatenating the modules should be named
+define('amd-window', function () {
+"use strict";
+
+    return window;
+});
